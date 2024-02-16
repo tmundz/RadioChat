b925b159667f36917d65c4f4c7ca18ba003a7bf3
Travis <mundy.travis@gmail.com>
2024-02-15 17:37:33 -0700

    React Login Set up

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,20 +1,33 @@
 import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 
-export const Login = (props) => {
+export const Login = () => {
     const navigate = useNavigate();
     const [user_name, setUserName] = useState('');
     const [password, setPassword] = useState('');
 
     // POST check login 
-    const handleSubmit = (e) => {
-        
-        //if  login success 
-        // need to set up the backend calls
-        //get pages
-        navigate("/users");
-        console.log(user_name);
-    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    user_name,
+                    password
+                })
+            });
+            if (!response.ok) {
+                throw new Error('User Does Not Found Or Password Not Correct')
+            }
+            const data = await response.json();
+            console.log('Submission successful', data);
+            navigate("/users")
+    } catch (error) {
+        console.error("Error during form submision: ", error);
+    }
+};
 
     // Get  page
     const handleReg = (e) => {
@@ -24,12 +37,12 @@ export const Login = (props) => {
     return (
         <>
         <div className="auth-form-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="user_name">User Name</label>
                 <input value={user_name} onChange={(e) => setUserName(e.target.value)} type="user_name" id="user_name" name="user_name"/>
                 <label htmlFor="Password">Password</label>
                 <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password"/>
-                <button onClick={() => handleSubmit()}>Submit</button>
+                <button type="submit">Submit</button>
             </form>
             <button onClick={() => handleReg()}>Dont have an account? Register here.</button>
         </div>
